20c1b1ef3fc179813819bdf72eed753aa7dbb984
Marina <marinamsm@live.com>
2018-12-09 15:36:43 -0200

    Tipos novos

---
+++ src/main/java/types/DOUBLE.java
@@ -0,0 +1,14 @@
+package types;
+
+public class DOUBLE extends Type {
+
+    public static final DOUBLE T = new DOUBLE();
+
+    private DOUBLE() {
+    }
+
+    @Override
+    public String toString() {
+        return "double";
+    }
+}
