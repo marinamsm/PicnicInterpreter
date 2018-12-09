aab2e26e5ed389d49bbb5743e073ec119eaf1c2a
Marina <marinamsm@live.com>
2018-12-09 15:37:17 -0200

    Novas expressões

--- src/main/java/absyn/ExpString.java
+++ src/main/java/absyn/ExpString.java
@@ -1,4 +1,28 @@
 package absyn;
 
-public class ExpString {
+import env.Env;
+import io.vavr.collection.Tree;
+import types.STRING;
+import types.Type;
+import types.VOID;
+
+public class ExpString extends Exp {
+    public final String value;
+
+    public ExpString(Loc loc, String value) {
+        super(loc);
+        this.value = value;
+    }
+
+    @Override
+    public Tree.Node<String> toTree() {
+        return Tree.of(annotateType("ExpString: " + value));
+    }
+
+    @Override
+    protected Type semantic_(Env env) {
+        // COMPLETE THIS CODE AND FIX THE RETURNED VALUE
+        return VOID.T;
+    }
+
 }
